7d3e8b8fee8cc44a0ae7ac4e4fecfb0a2a902a5b
WalkingLibrary <jumboofawarrior@gmail.com>
2022-03-06 00:17:33 -0700

    Adjustment -Refactoring Code and Files -Adding Comments

--- NET Solution Developer Question 11/Controllers/NetsolutionController.cs
+++ NET Solution Developer Question 11/Controllers/NetsolutionController.cs
@@ -3,7 +3,7 @@ using System.Text.Encodings.Web;
 
 namespace MvcMovie.Controllers
 {
-    public class HelloWorldController : Controller
+    public class NetSolutionController : Controller
     {
         // 
         // GET: /HelloWorld/
